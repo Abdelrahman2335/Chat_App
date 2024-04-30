37ef0e7830b049b41470f9ec941637fb0d0fe5d0
Abdelrahman2335 <125411627+Abdelrahman2335@users.noreply.github.com>
2024-04-30 05:40:39 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
+import 'firebase_optionsdart pub global activate flutterfire_cli.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
