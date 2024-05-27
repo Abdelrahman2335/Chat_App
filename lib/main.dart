ddbb0fc21d421ac8c64b567e489d46e3ccf79921
Abdelrahman2335 <125411627+Abdelrahman2335@users.noreply.github.com>
2024-05-27 06:59:12 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/layout.dart';
+import 'package:chat_app/provider/provider.dart';
 import 'package:chat_app/screens/info_screen.dart';
 import 'package:chat_app/screens/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,10 +17,10 @@ void main() async {
   runApp(const MyApp());
 }
 
-var myColorScheme =
-    ColorScheme.fromSeed(seedColor : Colors.blue, brightness: Brightness.light);
-var myDarkColorScheme =
-    ColorScheme.fromSeed(seedColor: Colors.indigo, brightness: Brightness.dark);
+ColorScheme myColorScheme =
+    ColorScheme.fromSeed(seedColor : const Color.fromARGB(3, 252, 252,250), brightness: Brightness.light);
+ColorScheme myDarkColorScheme =
+    ColorScheme.fromSeed(seedColor: Colors.redAccent, brightness: Brightness.dark,);
 
 
 class MyApp extends StatelessWidget {
@@ -26,27 +28,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      themeMode: ThemeMode.light,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData().copyWith(colorScheme: myColorScheme),
-      darkTheme: ThemeData().copyWith(colorScheme: myDarkColorScheme),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.userChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
+    return ChangeNotifierProvider(
+      create: (context) => ProviderApp(),
+      child: Consumer<ProviderApp>(
+        builder: (context, value, child) => GetMaterialApp(
+          themeMode: value.themeMode,
+          /// Note that value = Provider.of<ProviderApp>(context).themeMode.
+          /// But we can't we if here so we used Consumer.
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData().copyWith(colorScheme: myColorScheme),
+          darkTheme: ThemeData().copyWith(colorScheme: myDarkColorScheme,scaffoldBackgroundColor:myDarkColorScheme.background),
 
-            if (FirebaseAuth.instance.currentUser!.displayName == null) {
+          home: StreamBuilder(
 
-              return const InfoScreen();
-            } else  {
-              return const LayOutApp();
-            }
-          } else {
-            return const LoginScreen();
-          }
-
-        },
+            stream: FirebaseAuth.instance.userChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                if (FirebaseAuth.instance.currentUser!.displayName == null) {
+                  return const InfoScreen();
+                } else {
+                  return const LayOutApp();
+                }
+              } else {
+                return const LoginScreen();
+              }
+            },
+          ),
+        ),
       ),
     );
   }
